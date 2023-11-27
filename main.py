2b54b49e03d6baa3a75f016cdca8acfe47d8737c
CarlosVergelBarraza <116093582+CarlosVergelBarraza@users.noreply.github.com>
2023-11-26 23:29:52 -0500

    changer host

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ if __name__ == '__main__':
         db.create_all()
     asyncio.run(noseCompaeEstoyCansado())
     
-    app.run(debug=True,port=6694)
+    app.run(debug=True,host='0.0.0.0',port=6694)
