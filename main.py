7ef89c14a5ea120534f17be49ccfd97cfb5b9a8b
CarlosVergelBarraza <116093582+CarlosVergelBarraza@users.noreply.github.com>
2023-11-26 23:34:58 -0500

    add optional port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,6 +11,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:A*Caae55122f3g3G*A-FbBAeCg4Agb15@roundhouse.proxy.rlwy.net:16253/railway'#postgresql://h0l4dmin:kwkMIlPzfWhDBjFhe2CR@hola-bd-qa.ckeqxfmdqgne.us-east-1.rds.amazonaws.com/incoporacion
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'senku'
+# Obtén el puerto de la variable de entorno, o utiliza 5000 como valor por defecto
+port = int(os.environ.get('PORT', 5000))
 
 CORS(app)
 # Configuración de la base de datos
@@ -29,4 +32,4 @@ if __name__ == '__main__':
         db.create_all()
     asyncio.run(noseCompaeEstoyCansado())
     
-    app.run(debug=True,host='0.0.0.0',port=6694)
+    app.run(debug=True,host='0.0.0.0',port=port)
